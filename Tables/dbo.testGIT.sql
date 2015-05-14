c23770857b301f0fa0f62bbbcc9936a3035cc366
alevanRedGate <allen.levan@red-gate.com>
2015-05-14 14:45:16 -0700

    test

---
+++ Tables/dbo.testGIT.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[testGIT]
+(
+[test] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[tes1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
